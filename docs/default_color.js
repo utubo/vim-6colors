0f0a7bf6eeb7a2b5c4d89f3620e60e7fb272b0ca
utubo <utbutb@gmail.com>
2021-08-22 12:43:16 +0900

    1st commit

---
+++ docs/default_color.js
@@ -0,0 +1,252 @@
+const DEFAULT=`" *colorschme-name-here*
+" Author: ***
+" GitHub: ***
+" Notes:
+" Thx: This is based on https://github.com/ggalindezb/vim_colorscheme_template
+"
+highlight clear
+if exists('syntax_on')
+  syntax reset
+endif
+let s:colors_name = expand('<sfile>:t:r')
+let g:colors_name = s:colors_name
+let s:background = &background
+
+" COLORS
+"   <color-type><opacity>
+" color-type:
+"   n: normal
+"   b: positiv1(blue)
+"   g: positiv2(green)
+"   y: warn(yellow)
+"   r: error(red)
+" opacity:
+"   0: 0% default bg (only n0)
+"   1: 40% bg
+"   2: 60% middle fg
+"   3: 80% default fg
+"   4: 100% Highlight (eg. b4=cursor,g4=search)
+"   9: 100% Highlight on &background == 'light'
+if has('gui_running')
+  let s:term = 'gui'
+  let s:n0 = '#2d3037'
+  let s:n1 = '#505354'
+  let s:n2 = '#88a8aa'
+  let s:n3 = '#ccdddd'
+  let s:n4 = '#ffffff'
+  let s:b1 = '#2a3f51'
+  let s:b2 = '#376992'
+  let s:b3 = '#4492d3'
+  let s:b4 = '#4daeff'
+  let s:b9 = '#4daeff'
+  let s:g1 = '#2c4f47'
+  let s:g2 = '#3b9179'
+  let s:g3 = '#4ad3aa'
+  let s:g4 = '#55ffcc'
+  let s:g9 = '#55ffcc'
+  let s:y1 = '#4e432a'
+  let s:y2 = '#907232' " not used
+  let s:y3 = '#d2a039'
+  let s:y4 = '#ffc03e'
+  let s:y9 = '#ffc03e'
+  let s:r1 = '#4e2e36'
+  let s:r2 = '#903e50' " not used
+  let s:r3 = '#d24e69'
+  let s:r4 = '#ff597a'
+  let s:r9 = '#ff597a'
+else
+  set t_Co=256
+  let s:term = 'cterm'
+  let s:n0 = '236'
+  let s:n1 = '240'
+  let s:n2 = '109'
+  let s:n3 = '152'
+  let s:n4 = '15'
+  let s:b1 = '238'
+  let s:b2 = '67'
+  let s:b3 = '74'
+  let s:b4 = '75'
+  let s:b9 = '75'
+  let s:g1 = '238'
+  let s:g2 = '72'
+  let s:g3 = '79'
+  let s:g4 = '86'
+  let s:g9 = '86'
+  let s:y1 = '237'
+  let s:y2 = '137' " not used
+  let s:y3 = '179'
+  let s:y4 = '221'
+  let s:y9 = '221'
+  let s:r1 = '238'
+  let s:r2 = '131' " not used
+  let s:r3 = '168'
+  let s:r4 = '204'
+  let s:r9 = '204'
+endif
+
+if &background == 'light'
+  let s:background = 'light'
+  let [s:n0, s:n1, s:n3, s:n4] = [s:n4, s:n3, s:n1, s:n0]
+  let [s:b1, s:b3, s:b4] = [s:b3, s:b1, s:b9]
+  let [s:g1, s:g3, s:g4] = [s:g3, s:g1, s:g9]
+  let [s:y1, s:y3, s:y4] = [s:y3, s:y1, s:y9]
+  let [s:r1, s:r3, s:r4] = [s:r3, s:r1, s:r9]
+endif
+
+let s:fg     = s:term.'fg='
+let s:bg     = s:term.'bg='
+let s:style  = s:term.'='
+let s:none   = s:style.'NONE'
+let s:bold   = s:style.'bold'
+let s:italic = s:style.'italic'
+
+exe 'hi Normal'        s:fg s:n3 s:bg s:n0
+exe 'hi Cursor'        s:fg s:n4 s:bg s:b4
+exe 'hi CursorIM'      s:fg s:n4 s:bg s:y4
+exe 'hi CursorLine'    s:bg s:n1
+exe 'hi LineNr'        s:fg s:n2 s:bg s:n1
+exe 'hi CursorLineNR'  s:fg s:n4 s:bg s:n2
+exe 'hi CursorLine'    s:bg s:n1
+
+" -----------------
+" - Number column -
+" -----------------
+exe 'hi CursorColumn'  s:bg s:n1
+exe 'hi FoldColumn'    s:bg s:n1
+exe 'hi SignColumn'    s:fg s:g4 s:bg s:n1
+exe 'hi Folded'        s:fg s:n2 s:bg s:n0
+
+" -------------------------
+" - Window/Tab delimiters -
+" -------------------------
+exe 'hi VertSplit'     s:bg s:n1
+exe 'hi ColorColumn'   s:fg s:n2 s:bg s:n1
+exe 'hi TabLine'       s:fg s:n2 s:bg s:n1
+exe 'hi TabLineFill'   s:bg s:n1
+exe 'hi TabLineSel'    s:fg s:g3 s:bg s:n0 s:bold
+
+" -------------------------------
+" - File Navigation / Searching -
+" -------------------------------
+exe 'hi Directory'     s:fg s:g4
+exe 'hi Search'        s:fg s:n0 s:bg s:g4 s:bold
+hi! link IncSearch Cursor
+
+" -----------------
+" - Prompt/Status -
+" -----------------
+exe 'hi StatusLine'    s:fg s:n2 s:bg s:n1
+exe 'hi StatusLineNC'  s:fg s:n3 s:bg s:n2
+exe 'hi Question'      s:fg s:y4 s:bg s:n0 s:none
+exe 'hi Title'         s:fg s:b3 s:bg s:n0 s:bold
+exe 'hi ModeMsg'       s:fg s:n3 s:bg s:n0 s:none
+exe 'hi MoreMsg'       s:fg s:n2 s:bg s:n0 s:none
+
+" --------------
+" - Visual aid -
+" --------------
+exe 'hi MatchParen'    s:fg s:b4 s:bg s:n0
+exe 'hi Visual'        s:fg s:n4 s:bg s:b2 s:none
+exe 'hi VisualNOS'     s:fg s:n2 s:bg s:b3 s:none
+exe 'hi NonText'       s:fg s:n1 s:none
+
+exe 'hi Todo'          s:fg s:b4 s:bg 'NONE' s:bold
+exe 'hi Underlined'    s:fg s:b4
+exe 'hi Error'         s:fg s:r4 s:bg 'NONE' s:bold
+exe 'hi ErrorMsg'      s:fg s:r3 s:bg 'NONE' s:none
+exe 'hi WarningMsg'    s:fg s:y3
+hi! link Ignore NonText
+hi! link SpecialKey NonText
+
+" --------------------------------
+" Variable types
+" --------------------------------
+exe 'hi Function'      s:fg s:b2
+exe 'hi Identifier '   s:fg s:n3
+exe 'hi Constant'      s:fg s:b2
+exe "hi String"        s:fg s:b2 s:bg s:b1
+exe 'hi Character'     s:fg s:b2
+exe 'hi Number'        s:fg s:b2
+hi! link Boolean Number
+hi! link Float Number
+
+" --------------------------------
+" Language constructs
+" --------------------------------
+exe 'hi Statement'     s:fg s:b3
+exe 'hi Conditional'   s:fg s:b3
+exe 'hi Repeat'        s:fg s:b3
+exe 'hi Label'         s:fg s:b3 s:bold
+exe 'hi Keyword'       s:fg s:n2
+exe 'hi Exception'     s:fg s:y3
+exe 'hi Comment'       s:fg s:g2
+exe 'hi SpecialComment' s:fg s:g2 s:italic
+exe 'hi SpecialChar'   s:fg s:b3
+exe 'hi Tag'           s:fg s:g3
+exe 'hi Delimiter'     s:fg s:y4
+exe 'hi Debug'         s:fg s:n2
+hi! link Operator Normal
+hi! link Special Statement
+
+" ----------
+" - C like -
+" ----------
+exe 'hi PreProc' s:fg s:b3
+
+
+" --------------------------------
+" Diff
+" --------------------------------
+exe 'hi DiffAdd'       s:fg s:b1 s:bg s:b3
+exe 'hi DiffChange'    s:fg s:y1 s:bg s:y3
+exe 'hi DiffDelete'    s:fg s:r2 s:bg s:r3
+exe 'hi DiffText'      s:fg s:g1 s:bg s:g3 s:none
+
+" --------------------------------
+" Completion menu
+" --------------------------------
+exe 'hi Pmenu'         s:fg s:n1 s:bg s:b2
+hi! link PmenuSel Cursor
+exe 'hi PmenuSbar'     s:bg s:b1
+exe 'hi PmenuThumb'    s:bg s:b3
+
+" --------------------------------
+" Spelling
+" --------------------------------
+exe 'hi SpellBad'      s:fg s:r3 s:bg s:r1 s:style 'undercurl'
+exe 'hi SpellCap'      s:fg s:y3 s:bg s:n0 s:style 'undercurl'
+exe 'hi SpellLocal'    s:fg s:g3 s:bg s:n0
+exe 'hi SpellRare'     s:fg s:b3 s:bg s:n0
+
+" you can do it
+
+"-------------------------------------------------
+" Specific settings
+"-------------------------------------------------
+exe 'hi EasyMotionShadeDefault' s:fg s:n2 s:bg s:n0
+exe 'hi EasyMotionTarget' s:fg s:b4 s:bg s:n0
+exe 'hi EasyMotionTarget2First' s:fg s:g4 s:bg s:n0
+hi! link EasyMotionTarget2Second EasyMotionTarget2First
+
+exe 'hi ALEErrorSign' s:fg s:r4 s:bg s:n1
+exe 'hi ALEWarningSign' s:fg s:y4 s:bg s:n1
+
+function s:CustomSyntax()
+  if g:colors_name != s:colors_name
+    exe 'augroup CustomSyntax_' . s:colors_name . '|au!|augroup END'
+    return
+  endif
+  exe 'hi GitGutterAdd' s:fg s:g3 s:bg s:n1
+  exe 'hi GitGutterChange' s:fg s:y3 s:bg s:n1
+  exe 'hi GitGutterDelete' s:fg s:r3 s:bg s:n1
+  hi! link diffAdded DiffAdd
+  hi! link diffChanged DiffChange
+  hi! link diffRemoved DiffDelete
+endfunction
+
+exe 'augroup CustomSyntax_' . s:colors_name . '|au!|au ColorScheme,Syntax * call <SID>CustomSyntax()|augroup END'
+call s:CustomSyntax()
+
+exe 'set background='.s:background
+`;
+
---
+++ docs/script.js
@@ -0,0 +1,240 @@
+const styleSheet = document.styleSheets[0];
+const dest = document.getElementById('dest');
+const cbBackground = document.getElementById('cb_background');
+const cbN0 = document.getElementById('cb_n0');
+const cbN4 = document.getElementById('cb_n4');
+
+const colorsGui = {};
+const colorsCterm = {};
+
+const init = () => {
+  const fragment = document.createDocumentFragment();
+  const letColorReg = /let s:([a-z]\d) = '([^']+)'/;
+  const fgReg = /s:fg s:([a-z]\d)/;
+  const bgReg = /s:bg s:([a-z]\d)/;
+  const hiReg = /exe 'hi (\S+)'/;
+  const linkReg = /hi! link (\S+) (\S+)/;
+  const hiMap = {};
+  let isGui = true;
+  for (let line of DEFAULT.split('\n')) {
+    const span = document.createElement('SPAN');
+    let hiName;
+    let className = 'line';
+    let subItem;
+    if (hiReg.test(line)) {
+      hiName = RegExp.$1;
+      if (fgReg.test(line)) {
+        className += ` fg-${RegExp.$1}`;
+      }
+      if (bgReg.test(line)) {
+        className += ` bg-${RegExp.$1}`;
+      }
+      if (line.includes('s:bold')) {
+        className += ' bold';
+      }
+      if (line.includes('s:italic')) {
+        className += ' italic';
+      }
+    } else if (linkReg.test(line)) {
+      hiName = RegExp.$1;
+      className = hiMap[RegExp.$2];
+    } else if (isGui && line.includes("let s:term = 'cterm'")) {
+      isGui = false;
+    } else if (letColorReg.test(line)) {
+      const colorName = RegExp.$1;
+      const value = RegExp.$2;
+      span.id = (isGui ? 'gui_' : 'cterm_') + colorName;
+      subItem = document.createElement('SPAN');
+      subItem.className = 'color-thumb';
+      if (isGui) {
+        colorsGui[colorName] = value;
+        subItem.style.background = value;
+      } else {
+        colorsCterm[colorName] = value;
+        subItem.style.background = termColors[value].hex;
+      }
+    }
+    span.className = className;
+    span.textContent = line;
+    if (subItem) {
+      span.appendChild(subItem);
+    }
+    if (hiName) {
+      hiMap[hiName] = className;
+    }
+    fragment.appendChild(span);
+  }
+  dest.replaceChild(fragment, dest.firstChild);
+  for (let c of ['n0', 'n4', 'b4', 'g4', 'y4', 'r4']) {
+    const ci = document.getElementById('ci_' + c);
+    const value = colorsGui[c];
+    ci.value = value;
+  }
+};
+
+// Apply color
+const quoteReg = /'([^']+)'/;
+const lightColors = {
+  n0: 'n4', n1: 'n3', n3:'n1', n4: 'n0',
+  b1: 'b3', b3:'b1', b4: 'b9',
+  g1: 'g3', g3:'g1', g4: 'g9',
+  y1: 'y3', y3:'y1', y4: 'y9',
+  r1: 'r3', r3:'r1', r4: 'r9',
+};
+const applyColor = () => {
+  for (let i = styleSheet.cssRules.length - 1; 0 <= i; i --) {
+    styleSheet.deleteRule(i);
+  }
+  for (let [c, value] of Object.entries(colorsGui)) {
+    let v = value;
+    if (cbBackground.checked) {
+      v = colorsGui[lightColors[c] || c];
+    }
+    styleSheet.insertRule(`.fg-${c} { color: ${v}; }`);
+    styleSheet.insertRule(`.bg-${c} { background: ${v}; }`);
+    const span = document.getElementById(`gui_${c}`);
+    if (!span) continue;
+    span.firstChild.nodeValue = span.textContent.replace(quoteReg, `'${value}'`);
+    span.getElementsByClassName('color-thumb')[0].style.background = value;
+  }
+  for (let [c, value] of Object.entries(colorsCterm)) {
+    const span = document.getElementById(`cterm_${c}`);
+    if (!span) continue;
+    span.firstChild.nodeValue = span.textContent.replace(quoteReg, `'${value}'`);
+    span.getElementsByClassName('color-thumb')[0].style.background = termColors[value].hex;
+  }
+  cbN0.setAttribute('for', cbBackground.checked ? 'ci_n4' : 'ci_n0');
+  cbN4.setAttribute('for', cbBackground.checked ? 'ci_n0' : 'ci_n4');
+};
+
+// Color-buttons
+const applyOneColor = (colorName, value) => {
+  const ci = document.getElementById('ci_' + colorName);
+  if (ci && ci.value !== value) {
+    ci.value = value;
+  }
+  const c = colorName[0];
+  if (colorName[1] === '4') {
+    if (c !== 'n') {
+      colorsGui[c + '9'] = value;
+    }
+    const c4RGB = hexToRGB(value);
+    const n0RGB = hexToRGB(colorsGui.n0);
+    const opacities = [0, 0.4, 0.6, 0.8, 1];
+    for (let i = 0; i < 5; i++) {
+      let rgb = '#';
+      const opacity = opacities[i];
+      for (let j of ['r', 'g', 'b']) {
+        let mergeValue = 0;
+        mergeValue += n0RGB[j] * (1 - opacity);
+        mergeValue += c4RGB[j] * opacity;
+        mergeValue = Math.floor(mergeValue);
+        mergeValue = Math.min(mergeValue, 255);
+        rgb += ('0' + mergeValue.toString(16)).slice(-2);
+      }
+      colorsGui[c + i] = rgb;
+    }
+  }
+  for (let i of [0, 1, 2, 3, 4, 9]) {
+    const key = c + i;
+    if (colorsGui[key]) {
+      colorsCterm[key] = findTermColor(colorsGui[key]).index;
+    }
+  }
+};
+const onInputColorLazy = () => {
+  const colorName = onInputColorTarget.id.replace('ci_', '');
+  const value = onInputColorTarget.value;
+  if (colorsGui[colorName] === value) return;
+  colorsGui[colorName] = value;
+  applyOneColor(colorName, value);
+  applyColor();
+};
+let onInputColorTimer;
+let onInputColorTarget;
+const onInputColor = e => {
+  clearTimeout(onInputColorTimer);
+  onInputColorTarget = e.target;
+  onInputColorTimer = setTimeout(onInputColorLazy, 100);
+};
+addEventListener('input', e=> {
+  if (!e.target) return;
+  if (!e.target.classList) return;
+  if (e.target.classList.contains('color-input')) {
+    onInputColor(e);
+  }
+});
+
+// Tool buttons
+cbBackground.checked = false;
+cbBackground.addEventListener('click', applyColor);
+
+document.getElementById('file_pic').addEventListener('input', e => {
+  let r = new FileReader();
+  r.onload = e2 => {
+    let img = new Image();
+    img.onload = e3 => {
+      const w = e3.target.width;
+      const h = e3.target.height;
+      const canvas = document.getElementById('work_canvas');
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(e3.target, 0, 0, canvas.width, canvas.height);
+      let picks = [];
+      for (let x = 0; x < canvas.width; x ++) {
+        for (let y = 0; y < canvas.height; y ++) {
+          const data = ctx.getImageData(x, y, 1, 1).data;
+          const c = {
+            r: data[0],
+            g: data[1],
+            b: data[2],
+          };
+          if (!c.r && !c.g && !c.b) continue;
+          Object.assign(c, rgbToHSL(c));
+          picks.push(c);
+        }
+      }
+      const rgbToHex = rgb => {
+        return '#' +
+          ('0' + rgb.r.toString(16)).slice(-2) +
+          ('0' + rgb.g.toString(16)).slice(-2) +
+          ('0' + rgb.b.toString(16)).slice(-2);
+      };
+      const n = function(a, b) {
+        return Math.floor(picks.length * a / b);
+      };
+      picks.sort((a, b) => b.l - a.l);
+      const n0 = picks.slice(-1)[0];
+      const n4 = picks[0];
+      applyOneColor('n0',  rgbToHex(n0));
+      applyOneColor('n4',  rgbToHex(n4));
+      const minL = (n4.l - n0.l) * 2 / 5 + n0.l;
+      const maxL = (n4.l - n0.l) * 4 / 5 + n0.l;
+      picks = picks.filter(c => (minL < c.l && c.l < maxL));
+      picks.sort((a, b) => b.s - a.s);
+      const minS = picks[0].s / 4;
+      picks = picks.filter(c => minS < c.s);
+      picks.sort((a, b) => a.h - b.h);
+      applyOneColor('b4', rgbToHex(picks[n(3, 4)]));
+      applyOneColor('g4', rgbToHex(picks[n(2, 4)]));
+      applyOneColor('y4', rgbToHex(picks[n(1, 4)]));
+      applyOneColor('r4', rgbToHex(picks[0]));
+      applyColor();
+    };
+    img.src = e2.target.result;
+  };
+  r.readAsDataURL(e.target.files[0]);
+});
+
+document.getElementById('download').addEventListener('click', e=> {
+  const link = document.getElementById('downloadLink');
+  const lines = [];
+  for (let line of dest.getElementsByClassName('line')) {
+    lines.push(line.textContent);
+  }
+  link.href = 'data:text/plain;charset=utf8,' + encodeURIComponent(lines.join('\n'));
+  link.click();
+});
+
+init();
+applyColor();
+
