669c1ecb9a060652602f9e78f37ba585ce2b022c
utubo <utbutb@gmail.com>
2021-09-27 23:30:26 +0900

    タブラインを調整

--- docs/default_color.js
+++ docs/default_color.js
@@ -123,9 +123,9 @@ exe 'hi Folded'        s:fg s:n2 s:bg s:n0
 " -------------------------
 exe 'hi VertSplit'     s:bg s:n1
 exe 'hi ColorColumn'   s:fg s:n2 s:bg s:n1
-exe 'hi TabLine'       s:fg s:n2 s:bg s:n1 s:none
-exe 'hi TabLineFill'   s:bg s:n1 s:none
-exe 'hi TabLineSel'    s:fg s:g3 s:bg s:n0 s:bold
+exe 'hi TabLine'       s:fg s:n3 s:bg s:n2 s:none
+exe 'hi TabLineFill'   s:fg s:n3 s:bg s:n1 s:none
+exe 'hi TabLineSel'    s:fg s:b3 s:bg s:n0 s:bold
 
 " -------------------------------
 " - File Navigation / Searching -
